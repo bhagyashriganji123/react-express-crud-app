3e563dbe6ab53713b0a8fcab4d69c9759dbdb250
bhagyashriganji <104623228+bhagyashriganji@users.noreply.github.com>
2022-07-05 16:23:36 +0530

    initial code dump

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddUsers from './components/AddUsers';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
+import EditUser from './components/EditUser';
+
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/addUser' element={<AddUsers/>}/>
+            <Route path='/editUser' element={<EditUser/>}/>
+        </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
